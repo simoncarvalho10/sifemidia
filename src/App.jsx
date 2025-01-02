0434b86b3e63a73a1a1d2857119f75a25da7512f
simon ferreira de carvalho <simoncarvalho10>
2025-01-02 12:16:28 -0300

    ajuste galeria fotos 2

--- src/App.jsx
+++ src/App.jsx
@@ -249,11 +249,13 @@ useEffect(() => {
                                 //alt={`Image ${index + 1}`}
                                 className={`absolute block h-full md:w-full object-cover bg-cover bg-center -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 ${
                                     index === currentIndex 
-                                    ? 'opacity-100'
-                                    : index < currentIndex 
-                                    ? 'opacity-0'
-                                    : index < currentIndex }`}
-                            />
+                                    /*? 'opacity-100' : 'opacity-0'*/
+                                    ? 'translate-x-50'
+                                    : index < currentIndex
+                                    ? '-translate-x-full'
+                                    : 'translate-x-full'
+                                }`}
+                             />
                         ))}
                         </div>
                 </div>
@@ -270,7 +272,7 @@ useEffect(() => {
                         </button>
                     ))}
                 </div>
-                <button onClick={prevImage} type="button" class="absolute top-0 start-0 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+                <button onClick={prevImage} type="button" class="hidden md:block absolute top-0 start-0 items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
                     <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
                         <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
@@ -278,7 +280,7 @@ useEffect(() => {
                         <span class="sr-only">Previous</span>
                     </span>
                 </button>
-                <button onClick={nextImage} type="button" class="absolute top-0 end-0 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+                <button onClick={nextImage} type="button" class="hidden md:block absolute top-0 end-0 items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
                     <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
                         <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
